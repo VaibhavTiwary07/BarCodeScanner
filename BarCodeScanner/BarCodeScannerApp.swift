716bd06a233c991f87b60d0b44e468a9863b0b8b
vticr7 <vti9227@gmail.com>
2025-04-22 10:31:21 +0530

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Vaibhav  Tiwary on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
